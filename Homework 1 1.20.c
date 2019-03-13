b6f9d69cd1ff3041888802a1013e5870b1013cca
Wen Jingyu <48398548+wen0525@users.noreply.github.com>
2019-03-13 20:01:12 +0800

    Update Homework 1 1.20.c

--- Homework 1 1.20.c
+++ Homework 1 1.20.c
@@ -5,21 +5,20 @@ void main()
     int n;
     printf("n=");
     scanf("%d",&n);
-    float *a;
-    a=(float *)malloc(sizeof(float)*(n+1));
-    for(int i=n;i>=0;i--)
-    {
-        printf("a%d=",i);
-        scanf("%f",a+i);
-    }
     float x0;
     printf("x0=");
     scanf("%f",&x0);
+    float *a;
+    a=(float *)malloc(sizeof(float)*(n+1));
+    printf("a%d=",n);
+    scanf("%f",a+n);
     float p=*(a+n);
-    for(int j=n-1;j>=0;j--)
+    for(int i=n-1;i>=0;i--)
     {
+        printf("a%d=",i);
+        scanf("%f",a+i);
         p*=x0;
-        p+=*(a+j);
+        p+=*(a+i);
     }
     printf("P%d(x0)=%f\n",n,p);
     free(a);
